8978762cf2437c9ec66626e30462c0d76ecab34d
Shanmugalakshmi11 <shanmugalakshmi.chandrasekaran@docc.techstarter.de>
2024-04-08 09:10:14 +0200

    commit

---
+++ remainder/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Birthday Remainder </h1>
+    </div>
+  );
+}
+
+export default App;
